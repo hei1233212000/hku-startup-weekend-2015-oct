02de33013724dc0e3b7471584a3a407240e9a436
Leo To <tochiyeung88@gmail.com>
2015-10-18 10:35:42 +0800

    variables

--- HappyCorner/js/scripts.js
+++ HappyCorner/js/scripts.js
@@ -2,24 +2,43 @@
 //$(document).ready(function()
 {
 	// Define a "Person" class that tracks its own name and children, and has a method to add a new child
-	var Person = function(name, children) {
-		this.name = name;
-		this.children = ko.observableArray(children);
-	 
-		this.addChild = function() {
-			this.children.push("New child");
-		}.bind(this);
-	}
 
 	// The view model is an abstract description of the state of the UI, but without any knowledge of the UI technology (HTML)
 	var viewModel = {
 		list: [
-			new Person("Annabelle", ["Arnie", "Anders", "Apple"]),
-			new Person("Bertie", ["Boutros-Boutros", "Brianna", "Barbie", "Bee-bop"]),
-			new Person("Charles", ["Cayenne", "Cleopatra"]),
-			new Person("Charles", ["Cayenne", "Cleopatra"]),
-			new Person("Charles", ["Cayenne", "Cleopatra"])
-			],
+			{
+				district: "Mong Kok",
+				rating: 5,
+				price: 150,
+				fan: true,
+				ac: true,
+				wifi: false,
+				karaoke: false,
+				food: true,
+				recommend: true
+			},
+			{
+				district: "Tsim Sha Tsui",
+				rating: 4,
+				price: 100,
+				fan: true,
+				wifi:true,
+				karaoke: true,
+				recommend: true
+			},
+			{
+				district: "Central",
+				rating: 3
+			},
+			{
+				district: "Bertie",
+				rating: 4
+			},
+			{
+				district: "Bertie",
+				rating: 5
+			}
+			]
 		
 	};
 	
